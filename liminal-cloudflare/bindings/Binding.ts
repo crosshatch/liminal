7bab3f242ecf38bc13089e94e57963480f0d1f34
Harry Solovay <harrysolovay@gmail.com>
2026-04-22 18:22:14 -0400

    improve binding errors

--- liminal-cloudflare/bindings/Binding.ts
+++ liminal-cloudflare/bindings/Binding.ts
@@ -1,10 +1,14 @@
 import { env } from "cloudflare:workers"
-import { Data, Context, Layer, Effect, Schema as S, Cause, SchemaIssue, Option } from "effect"
+import { Data, Context, Layer, Effect } from "effect"
 
 const TypeId = "~liminal/cloudflare/Binding" as const
 
-export class BindingError extends Data.TaggedError("BindingError")<{
-  readonly cause: S.SchemaError | Cause.NoSuchElementError
+export class BindingMissingError extends Data.TaggedError("BindingMissingError")<{
+  readonly missing: string
+}> {}
+
+export class BindingInvalidError extends Data.TaggedError("BindingInvalidError")<{
+  readonly invalid: string
 }> {}
 
 export interface Binding<Self, Id extends string, A, ROut, E, RIn> extends Context.Service<Self, A> {
@@ -12,23 +16,29 @@ export interface Binding<Self, Id extends string, A, ROut, E, RIn> extends Conte
 
   readonly [TypeId]: typeof TypeId
 
-  readonly layer: (config: { binding: string }) => Layer.Layer<Self | ROut, BindingError | E, RIn>
+  readonly layer: (config: {
+    binding: string
+  }) => Layer.Layer<Self | ROut, BindingMissingError | BindingInvalidError | E, RIn>
 }
 
 export const Binding =
   <Self>() =>
   <Id extends string, A extends object, ROut = never, E = never, RIn = never>(
     id: Id,
-    f: (value: object) => value is A,
+    is: (value: object) => value is A,
     makeLayer: (resource: A) => Layer.Layer<ROut, E, RIn> = () => Layer.empty as never,
   ): Binding<Self, Id, A, ROut, E, RIn> => {
     const tag = Context.Service<Self, A>()(id)
     const layer = ({ binding }: { binding: string }) =>
-      Effect.fromNullishOr((env as never)[binding]).pipe(
-        Effect.filterOrFail(f, (v) => new S.SchemaError(new SchemaIssue.InvalidValue(Option.some(v)))),
-        Effect.catch((cause) => new BindingError({ cause }).asEffect()),
-        Effect.map((v) => Layer.mergeAll(makeLayer(v), Layer.succeed(tag, v))),
-        Layer.unwrap,
-      )
+      Effect.gen(function* () {
+        const v = (env as never)[binding]
+        if (!v) {
+          return yield* new BindingMissingError({ missing: binding })
+        }
+        if (!is(v)) {
+          return yield* new BindingInvalidError({ invalid: binding })
+        }
+        return Layer.mergeAll(Layer.succeed(tag, v), makeLayer(v))
+      }).pipe(Layer.unwrap)
     return Object.assign(tag, { [TypeId]: TypeId, layer })
   }
