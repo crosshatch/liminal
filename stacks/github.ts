03367d4cad8e685fba45121310e1b0cc582c5221
Harry Solovay <harrysolovay@gmail.com>
2026-05-29 19:05:58 -0400

    continued

--- stacks/github.ts
+++ stacks/github.ts
@@ -1,45 +1,22 @@
 import * as Alchemy from "alchemy"
-import * as Cloudflare from "alchemy/Cloudflare"
 import * as GitHub from "alchemy/GitHub"
-import { Config, Effect, Layer, Redacted } from "effect"
+import { Config, Effect, Redacted } from "effect"
 
 export default Alchemy.Stack(
   "liminal-github",
   {
-    providers: Layer.mergeAll(Cloudflare.providers(), GitHub.providers()),
+    providers: GitHub.providers(),
     state: Alchemy.localState(),
   },
   Effect.gen(function* () {
     const accountId = yield* Config.string("CLOUDFLARE_ACCOUNT_ID")
-    const { value } = yield* Cloudflare.AccountApiToken("DeployApiToken", {
-      name: "liminal-deploy",
-      accountId,
-      policies: [
-        {
-          effect: "allow",
-          permissionGroups: [
-            "Workers Scripts Write",
-            "Workers KV Storage Write",
-            "Workers R2 Storage Write",
-            "D1 Write",
-            "Queues Write",
-            "Pages Write",
-            "Account Settings Write",
-            "Secrets Store Write",
-            "Workers Tail Read",
-          ],
-          resources: {
-            [`com.cloudflare.api.account.${accountId}`]: "*",
-          },
-        },
-      ],
-    })
+    const apiToken = yield* Config.string("CLOUDFLARE_API_TOKEN")
     yield* GitHub.Secrets({
       owner: "crosshatch",
       repository: "liminal",
       environment: "deploy",
       secrets: {
-        CLOUDFLARE_API_TOKEN: value,
+        CLOUDFLARE_API_TOKEN: Redacted.make(apiToken),
         CLOUDFLARE_ACCOUNT_ID: Redacted.make(accountId),
       },
     })
