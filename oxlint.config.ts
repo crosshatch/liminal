eafe812eef9f79f095c742a78a2af2674acdd0f3
Harry Solovay <harrysolovay@gmail.com>
2026-07-12 12:38:54 -0400

    simplify oxlint config

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from "oxlint"
 
-import { baseConfig } from "./konfik/oxlint/baseConfig.ts"
+import { baseConfig, rest } from "./konfik/oxlint/baseConfig.ts"
 import { defineReactConfig } from "./konfik/oxlint/defineReactConfig.ts"
 
 export default defineConfig({
-  env: { browser: true },
   extends: [baseConfig, defineReactConfig(["examples/*/app/**/*"])],
-  ignorePatterns: ["**/*.gen.ts", "**/routeTree.gen.ts", "repos"],
-  jsPlugins: [
-    {
-      name: "custom",
-      specifier: "./konfik/oxlint/rules/index.ts",
-    },
-  ],
-  rules: {
-    "custom/require-readonly-type-members": "error",
-  },
+  ...rest(),
 })
