a768cbe73c27287d59f2c5efd03369c2c1719b60
Harry Solovay <4893548+harrysolovay@users.noreply.github.com>
2026-08-29 09:06:08 -0400

    update konfik

--- liminal/vitest.config.ts
+++ liminal/vitest.config.ts
@@ -1,7 +1,10 @@
 import { mergeConfig, type ViteUserConfig } from "vitest/config"
 
 import config from "../konfik/vitest.ts"
+import PackageJson from "./package.json" with { type: "json" }
 
 export default mergeConfig(config, {
-  test: { name: "liminal" },
+  test: {
+    name: PackageJson.name,
+  },
 } satisfies ViteUserConfig)
